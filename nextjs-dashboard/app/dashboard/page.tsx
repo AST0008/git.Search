6d01b23fa5faec5e86b410c0b77905251053d83f
AST <ashwajittayade30@gmail.com>
2024-08-29 23:29:06 +0530

    adding all files

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <p>Welcome to your dashboard!</p>
+        {/* Add more dashboard content here */}
+      </div>
+    );
+  }
