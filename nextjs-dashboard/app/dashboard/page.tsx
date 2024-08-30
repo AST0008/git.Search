a45ffe1360d8cb7f962ce1af64f5a9ab1b4ebbe1
AST <ashwajittayade30@gmail.com>
2024-08-31 00:15:39 +0530

    Added Invoice Page and Other stuff

--- nextjs-dashboard/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardPage() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">Dashboard</h1>
-        <p>Welcome to your dashboard!</p>
-        {/* Add more dashboard content here */}
-      </div>
-    );
-  }
